37b499ab89c4e2508b8bdf37865285652e087dea
Aditya <aditya.sasmal.dev@gmail.com>
2024-07-04 22:26:22 +0530

    feat: Add friends by email
    API endpoint for sending friend requests.

---
+++ src/app/api/friends/add/route.ts
@@ -0,0 +1,64 @@
+import { fetchRedis } from "@/helpers/redis";
+import { authOptions } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { addFriendValidator } from "@/lib/validators/add-friend";
+import { getServerSession } from "next-auth";
+import { z } from "zod";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const { email: emailToAdd } = addFriendValidator.parse(body.email);
+    const idToAdd = (await fetchRedis(
+      "get",
+      `user:email:${emailToAdd}`,
+    )) as string;
+
+    if (!idToAdd) {
+      return new Response("Invalid e-mail", { status: 400 });
+    }
+
+    // Get the user sending the friend request
+    const session = await getServerSession(authOptions);
+
+    if (!session) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+    if (idToAdd === session.user.id) {
+      return new Response("You cannont add yourself.", { status: 400 });
+    }
+
+    // check if user is already added
+    const isAlreadyAdded = (await fetchRedis(
+      "sismember",
+      `user:${idToAdd}:incoming_friend_requests`,
+      session.user.id,
+    )) as 0 | 1;
+
+    if (isAlreadyAdded) {
+      return new Response("Already added this user", { status: 400 });
+    }
+
+    // check if user is already friend
+    const isAlreadyFriends = (await fetchRedis(
+      "sismember",
+      `user:${session.user.id}:friends`,
+      idToAdd,
+    )) as 0 | 1;
+
+    if (isAlreadyFriends) {
+      return new Response("You are already friends!", { status: 400 });
+    }
+
+    // valid req, send friend req
+    await db.sadd(`user:${idToAdd}:incoming_friend_requests`, session.user.id);
+
+    return new Response("OK");
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return new Response("Invalid request payload", { status: 422 });
+    }
+
+    return new Response("Invalid Request", { status: 400 });
+  }
+}
