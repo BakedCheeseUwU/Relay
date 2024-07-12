8c8edae76d6f0e79f3418e06632ae6424a2f8bf7
Aditya <aditya.sasmal.dev@gmail.com>
2024-07-12 17:01:14 +0530

    Add global types

--- src/types/db.d.ts
+++ src/types/db.d.ts
@@ -1,6 +1,26 @@
 interface User {
-    id:string
-    name:string
-    email:string
-    image:string
+  id: string;
+  name: string;
+  email: string;
+  image: string;
 }
+
+interface Chat {
+  id: string;
+  messages: Message[];
+}
+
+interface Message {
+  id: string;
+  senderId: string;
+  recieverId: string;
+  text: string;
+  timestamp: number;
+}
+
+interface FriendRequest {
+  id: string;
+  senderId: string;
+  recieverId: string;
+}
+
