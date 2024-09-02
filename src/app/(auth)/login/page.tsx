58dadc26f9e1558f43a82b4b7f7cc60b444ff383
Aditya <aditya.sasmal.dev@gmail.com>
2024-09-03 03:45:54 +0530

    update: logo

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,6 +2,7 @@
 
 import Button from "@/components/ui/Button";
 import { signIn } from "next-auth/react";
+import Image from "next/image";
 import { FC, useState } from "react";
 import toast from "react-hot-toast";
 
@@ -26,7 +27,7 @@ const Page: FC = () => {
       <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="w-full flex flex-col items-center max-w-md space-y-8">
           <div className="flex flex-col items-center gap-8">
-            logo
+            <Image src="./logo.svg" alt="logo" height={56} width={56} />
             <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
               Sign in to your account
             </h2>
